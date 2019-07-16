ec0749e191da5592da848529819f7dbc41400689
efrainmejiasc <e.mejias@innova4j.com>
2019-07-16 14:13:47 -0400

    Update InitTest.py

--- TestPython/InitTest.py
+++ TestPython/InitTest.py
@@ -3,7 +3,11 @@ final = 11
 
 for x in range(numero,final):
    print("posicion " + str(x - numero))
-   print("caballo " + str(x))
+   print("caball0 " + str(x))
+   if x == 10:
+          continue
+msg ="Efrain Mejias Castillo"
+print (msg)
 
 
 
