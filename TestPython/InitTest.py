730907c178793b37a55cbbb00eca4e1b30d1ab8b
efrainmejiasc <efrainmejiasc@hotmail.com>
2019-07-18 08:08:06 -0400

    Update InitTest.py

--- TestPython/InitTest.py
+++ TestPython/InitTest.py
@@ -19,16 +19,28 @@ print (mydb)
 
 cnx = mysql.connector.connect(host='127.0.0.1',user='root',passwd='root', database='gig')
 cnxCursor = cnx.cursor()
-cnxCursor.execute("CREATE TABLE testpython (id INT NOT NULL AUTO_INCREMENT,name VARCHAR(255), apellido VARCHAR(255), edad INT , salario DOUBLE , fecha DATE,PRIMARY KEY (id))")
+ #cnxCursor.execute("CREATE TABLE testpython (id INT NOT NULL AUTO_INCREMENT,name VARCHAR(255), apellido VARCHAR(255), edad INT , salario DOUBLE , fecha DATE,PRIMARY KEY (id))")
 
 
 cnxCursor.execute("show databases")
 for tabla in cnxCursor:
     print(tabla)
 
+#cnxCursor.execute("INSERT INTO testpython ('name','apellido','edad','salario','fecha')VALUES('efrain','mejias castillo','47','300.45','Now()')")
 
 
-cnxCursor.close()
+try:
+  
+        sql = "INSERT INTO testpython ('name','apellido') VALUES (%s, %s, %s)"
+        try:
+            cnxCursor.execute(sql, ('efrain','mejias castillo'))
+            print("Task added successfully")
+        except NameError:
+            print(str(NameError))
+            
+finally:
+         cnxCursor.close()
+
 
 
 
